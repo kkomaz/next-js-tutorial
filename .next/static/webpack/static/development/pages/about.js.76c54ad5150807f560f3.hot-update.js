b38cf40c139b0de9a54b6ec96b1b9dd5c276afcb
Alex Lee <alex@uniteus.com>
2019-03-10 16:48:34 -0400

    Dynamic pages and shared components

---
+++ .next/static/webpack/static/development/pages/about.js.76c54ad5150807f560f3.hot-update.js
@@ -0,0 +1,18 @@
+webpackHotUpdate("static/development/pages/about.js",[
+/* 0 */,
+/* 1 */,
+/* 2 */
+false,
+/* 3 */
+/*!*************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fabout&absolutePagePath=%2FUsers%2Falexanderlee%2Fdevelopment%2Fpersonal%2Fnext%2Fhello-next%2Fpages%2Fabout.js ***!
+  \*************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fabout&absolutePagePath=%2FUsers%2Falexanderlee%2Fdevelopment%2Fpersonal%2Fnext%2Fhello-next%2Fpages%2Fabout.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=%2FUsers%2Falexanderlee%2Fdevelopment%2Fpersonal%2Fnext%2Fhello-next%2Fpages%2Fabout.js!./");
+
+
+/***/ })
+])
+//# sourceMappingURL=about.js.76c54ad5150807f560f3.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Link from 'next/link' // HOC
+import Layout from '../components/Layout'
+
+const PostLink = (props) => (
+  <li>
+    <Link
+      as={`/p/${props.id}`}
+      href={`/post?title=${props.title}`}
+    >
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
+
+const Index = () => (
+  <Layout>
+    <ul>
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+    </ul>
+  </Layout>
+)
+
+export default Index
